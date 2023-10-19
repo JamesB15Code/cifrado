e9544a7ed316318e1e1a39c1e35902ad4383e24e
James Brian <jamesbrian1504@gmail.com>
2023-10-19 01:44:36 -0600

    escitala

--- src/App.js
+++ src/App.js
@@ -1,87 +1,156 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import CryptoJS from "crypto-js";
 
-class App extends Component {
-  state = {
-    opcion: "encrypt", // Opción "encrypt" para cifrar, "decrypt" para descifrar
-    mensaje: "",
-    resultado: "",
-    clave: "miClaveSecreta", // Puedes cambiar esto a tu clave secreta
-  };
+function App() {
+  
+  const [message, setMessage] = useState("");
+  const [diametro, setDiametro] = useState();
+  const [lados, setLados] = useState();
+  const [result, setResult] = useState("");
+  const [matrixToShow, setMatrixToShow] = useState("");
 
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
+  const cifrarTexto = () => {
+    if (message && diametro && lados) {
+      const messageArray = message.split(" ").filter((word) => word);
+      const rows = Math.ceil(messageArray.join("").length / lados);
+      const encryptedMatrix = [];
 
-  handleClearText = () => {
-    this.setState({ mensaje: "", resultado: "" });
-  };
-
-  handleCifradoCesar = () => {
-    const { opcion, mensaje, clave } = this.state;
-
-    if (opcion === "encrypt") {
-      const ciphertext = CryptoJS.AES.encrypt(mensaje, clave).toString();
-      this.setState({ resultado: ciphertext });
-    } else if (opcion === "decrypt") {
-      try {
-        const bytes = CryptoJS.AES.decrypt(mensaje, clave);
-        const originalText = bytes.toString(CryptoJS.enc.Utf8);
-        this.setState({ resultado: originalText });
-      } catch (error) {
-        this.setState({
-          resultado: "Error al descifrar. Clave incorrecta o mensaje no válido.",
-        });
+      for (let i = 0; i < rows; i++) {
+        encryptedMatrix[i] = [];
       }
+
+      let rowIndex = 0;
+      let colIndex = 0;
+
+      for (const word of messageArray) {
+        for (const letter of word) {
+          encryptedMatrix[rowIndex][colIndex] = letter;
+          colIndex = (colIndex + 1) % lados;
+          rowIndex += colIndex === 0 ? 1 : 0;
+        }
+      }
+
+      const resultArray = [];
+
+      for (let col = 0; col < lados; col++) {
+        for (let row = 0; row < rows; row++) {
+          if (encryptedMatrix[row][col]) {
+            resultArray.push(encryptedMatrix[row][col]);
+          }
+        }
+      }
+
+      const result = resultArray.join(" ");
+      setResult(result);
+
+      // Establecer la matriz en el estado para mostrar
+      const matrixToShow = encryptedMatrix
+        .map((row) => row.join(" "))
+        .join("\n");
+      setMatrixToShow(matrixToShow);
     } else {
-      this.setState({
-        resultado:
-          "Opción no válida. Debe seleccionar una opción para cifrar o descifrar.",
-      });
+      setResult(
+        "Por favor, ingresa un mensaje, un diámetro y el número de lados del semicilindro."
+      );
     }
   };
 
-  render() {
-    const { opcion, mensaje, resultado } = this.state;
+  const descifrarTexto = () => {
+    if (message && diametro && lados) {
+      const messageArray = message.split(" ").filter((word) => word);
+      const rows = Math.ceil(messageArray.join("").length / lados);
+      const encryptedMatrix = [];
 
-    return (
-      <div className="App">
-        <h1>Cifrado CryptoJS</h1>
-        <label>
-          Opción:
-          <select
-            name="opcion"
-            value={opcion}
-            onChange={this.handleInputChange}
-          >
-            <option value="encrypt">Cifrar</option>
-            <option value="decrypt">Descifrar</option>
-          </select>
-        </label>
+      for (let i = 0; i < rows; i++) {
+        encryptedMatrix[i] = [];
+      }
 
-        <br />
-        <label>
-          Mensaje:
-          <textarea
-            type="text"
-            name="mensaje"
-            value={mensaje}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <button onClick={this.handleCifradoCesar}>Realizar</button>
-        <button onClick={this.handleClearText}>Limpiar Texto</button>
+      let rowIndex = 0;
+      let colIndex = 0;
 
-        <br />
-        <div>Resultado:
-          <textarea value={resultado}/>
-           </div>
+      for (const word of messageArray) {
+        for (const letter of word) {
+          encryptedMatrix[rowIndex][colIndex] = letter;
+          colIndex = (colIndex + 1) % lados;
+          rowIndex += colIndex === 0 ? 1 : 0;
+        }
+      }
+
+      const resultArray = [];
+
+      for (let col = 0; col < lados; col++) {
+        for (let row = 0; row < rows; row++) {
+          if (encryptedMatrix[row][col]) {
+            resultArray.push(encryptedMatrix[row][col]);
+          }
+        }
+      }
+
+      const result = resultArray.join(" ");
+      setResult(result);
+
+      // Establecer la matriz en el estado para mostrar
+      const matrixToShow = encryptedMatrix
+        .map((row) => row.join(" "))
+        .join("\n");
+      setMatrixToShow(matrixToShow);
+    } else {
+      setResult(
+        "Por favor, ingresa un mensaje, un diámetro y el número de lados del semicilindro."
+      );
+    }
+  };
+
+  return (
+    <div className="App2">
+      <h1>Escítala React App</h1>
+
+      <div className="input-container">
+        <p>Para su practica realiza este ejemplo, empleando 3 filas con 9 caracteres de longitud</p>
+        <p>Con la siguiente frase: <br/><br/>ejemplodelmetododelescitala</p>
+        <label className="diameter">Ingresa las filas:</label>
+        <input
+          type="number"
+          id="diameter"
+          placeholder="Ingresa las filas"
+          value={diametro}
+          onChange={(e) => setDiametro(e.target.value)}
+        />
       </div>
-    );
-  }
+
+      <div className="input-container">
+        <label className="sides">Ingresa el número de longitud:</label>
+        <input
+          type="number"
+          id="sides"
+          placeholder="Ingresa número de longitud"
+          value={lados}
+          onChange={(e) => setLados(e.target.value)}
+        />
+      </div>
+
+      <div className="input-container">
+        <label htmlFor="message">Mensaje a cifrar:</label>
+        <textarea
+          id="message"
+          placeholder="Ingresa el mensaje"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button className="btnEscitala" onClick={cifrarTexto}>
+          Cifrar
+        </button>
+        
+      </div>
+
+      <div className="">
+        <pre>{result}</pre>
+      </div>
+      <div className="matriz">
+        <textarea readOnly id="matrixToShow" value={matrixToShow} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
